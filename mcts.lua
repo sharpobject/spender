3719eb471bab98217541e45b9c3ca30741836fc3
Robert Burke <sharpobject@gmail.com>
2018-07-08 00:32:09 -0700

    mcts cleanup

--- mcts.lua
+++ mcts.lua
@@ -123,18 +123,18 @@ function MCTS:probs(state, temp)
   end
 end
 
-local assert = assert
-
-local function totable(tensor, len)
-  local P = tb_new(len, 0)
-  for i=1,len do
-    P[i] = tensor[i]
+function MCTS:search(state, s)
+  if state.result then
+    return -state.result
   end
-  return P
-end
-
-
-function MCTS:search_a(state, s, node)
+  local node = self.nodes[s]
+  if node == nil then
+    node = {
+      current_visits = 0
+    }
+    self.nodes[s] = node
+  end
+  if node.P == nil or node.current_visits > 0 then
     local ps, v = self.nnet_eval(state)
     local valids, nvalids = state:list_moves()
     local tab = tb_new(nvalids, 0)
@@ -159,9 +159,7 @@ function MCTS:search_a(state, s, node)
     node.nvalids = nvalids
     node.N = 0
     return -v
-end
-
-function MCTS:search_b(state, s, node)
+  end
   local nvalids = node.nvalids
   local P = node.P
   local Qi = node.Qi
@@ -201,90 +199,6 @@ function MCTS:search_b(state, s, node)
   state:apply_move(node.valids[best])
   node.current_visits = node.current_visits + 1
   local v = self:search(state, state:as_string())
-  node.current_visits = node.current_visits - 1
-  Qi[best] = (Ni[best] * Qi[best] + v) / (Ni[best] + 1)
-  Ni[best] = Ni[best] + 1
-  node.N = node.N + 1
-  return -v
-end
-
-function MCTS:search(state, s)
-  if state.result then
-    return -state.result
-  end
-  local node = self.nodes[s]
-  if node == nil then
-    node = {
-      current_visits = 0
-    }
-    self.nodes[s] = node
-  end
-  if node.P == nil or node.current_visits > 0 then
-    local junk = self:search_a(state, s, node) if junk then return junk end
-    local ps, v = self.nnet_eval(state)
-    local valids, nvalids = state:list_moves()
-    local sum = 0
-    for i=1,nvalids do
-      sum = sum + ps[valids[i]]
-      ps[i] = ps[valids[i]]
-    end
-    ps = ps:narrow(1, 1, nvalids)
-    if sum > 0 then
-      ps:div(sum)
-    else
-      print("All valid moves were masked, and yes I did copy this from alpha-zero-general")
-      ps:fill(1/nvalids)
-    end
-    node.P = ps
-    node.valids = valids
-    node.nvalids = nvalids
-    node.N = 0
-    return -v
-  end
-  local junk = self:search_b(state, s, node) if junk then return junk end
-  local nvalids = node.nvalids
-  local P = node.P
-  local Qi = node.Qi
-  local Ni = node.Ni
-  if not Qi then
-    Qi, Ni = tb_new(nvalids, 0), tb_new(nvalids, 0)
-    for i=1,nvalids do
-      Qi[i] = 0
-      Ni[i] = 0
-    end
-    node.Qi = Qi
-    node.Ni = Ni
-  end
-  local best_score = -1e99
-  local best = -1
-  local nbests = 1
-  local n_term = node.N
-  if n_term == 0 then
-    n_term = EPS
-  end
-  local cpuct_term = self.cpuct * sqrt(n_term)
-  for i=1,nvalids do
-    local u
-    local p = P[i]
-    u = Qi[i] + p * cpuct_term / (1+Ni[i])
-    if u > best_score then
-      best_score = u
-      best = i
-      nbests = 1
-    elseif u == best_score then
-      nbests = nbests + 1
-      if random() < 1/nbests then
-        best = i
-      end
-    end
-  end
-
-  local a = node.valids[best]
-  --print("mcts move "..a)
-  local next_state = GameState(state)
-  next_state:apply_move(a)
-  node.current_visits = node.current_visits + 1
-  local v = self:search(next_state, next_state:as_string())
   node.current_visits = node.current_visits - 1
   Qi[best] = (Ni[best] * Qi[best] + v) / (Ni[best] + 1)
   Ni[best] = Ni[best] + 1
